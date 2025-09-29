0a25c94121facfc1ee2caef54d5fc937551b6c10
macuna <marcoantonioraiacuna@gmail.com>
2025-09-29 06:27:23 -0400

    Feature ai functions completed.

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const authRouter = require('./auth');
 const projectRouter = require('./projects');
+const generateRouter = require("./generate");
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/projects', projectRouter);
+router.use("/ai", generateRouter);
 
 module.exports = router;
